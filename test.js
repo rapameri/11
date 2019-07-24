e5cb44af4c0c840be1e160f9625a511df16e1642
ラパパメリカ <36385910+rapameri@users.noreply.github.com>
2019-07-24 22:32:58 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,21 @@
+﻿//window.alert('数字を一つ思い浮かべてください');
+document.addEventListener('DOMContentLoaded',function(){
+  window.addEventListener('scroll',function(){
+    const scrollElem = document.scrollingElement || document.documentElement || document.body;
+    const s = scrollElem.scrollTop;
+    document.title = s;
+  });
+  (function(b){
+    //document.style.background = 
+  })(0);
+});
+function sample(x){
+  const screen = document.getElementById('mediascreen');
+  screen.querySelector('img#media-main').setAttribute('src','img/sample'+x+'.png');
+  screen.querySelector('#media-caption').innerHTML = '';
+  //event.target.parentNode.querySelector('.member-name').innerHTML;
+  screen.classList.add('show');
+}
+function closesample(){
+  document.getElementById('mediascreen').classList.remove('show');
+}
